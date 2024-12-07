43825ad100408a1a70592276ab1fc09e97c838ba
Iphat Chaudhary <iphatchaudhary0509@gmail.com>
2024-12-07 22:10:39 +0530

    new features is added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 
+# project
+This project is designed for learning purpose
--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/expressError.js");
+const {listingSchema} = require("./schema.js");
+// const { errorMonitor } = require("events");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -28,16 +32,23 @@ main()
 })
 .catch((err) => {
     console.log(err);
-})
-
-
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
 });
+
 
 app.get("/", (req,res) => {
     res.send("Hi I am Iphat");
 });
+
+const validateListing = (req, res, next) => {
+    let{ error} = listingSchema.validate(req.body);
+    console.log(error);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
 
 //Index Route
 app.get("/listings",async (req,res) => {
@@ -58,14 +69,35 @@ app.get("/listings/:id", async(req, res) => {
     res.render("listings/show.ejs", { listing });
   });
 
+
+//custom error handling is applied on "create route"  
 //Create Route
-app.post("/listings",async(req,res) => {
+app.post("/listings",validateListing,wrapAsync(async(req,res,next) => {//wrapAsync function is better way to write try & catch
+
+//either we use this
+// let result = listingSchema.validate(req.body);
+// console.log(result);
+//or this
+//    if(!newListing.title){
+//     throw new  ExpressError(400,"Title is missing");
+//    }
+//    if(!newListing.description){
+//     throw new  ExpressError(400,"Description is missing");
+//    }
+//    if(!newListing.location){
+//     throw new  ExpressError(400,"Location is missing");
+//    }
+// if(result.error) {
+//     throw new ExpressError(400,result.error);
+// }
+
 //this is one type to get value but another way is in "new.ejs"    
- // let{title, description, image, price, country, location} = req.body;
-   const newListing = new Listing(req.body.listing);
+  // let{title, description, image, price, country, location} = req.body;  
+  const newListing = new Listing(req.body.listing);
    await newListing.save();
    res.redirect("/listings");
-});
+})
+);
 
 //Edit Route
 app.get("/listings/:id/edit", async(req,res) => {
@@ -75,8 +107,11 @@ app.get("/listings/:id/edit", async(req,res) => {
 });
 
 //Update Route
-app.put("/listings/:id", async(req,res) => {
-    let {id} = req.params;
+app.put("/listings/:id",validateListing, async(req,res) => {
+    // if(!req.body.listing){
+    //     throw new ExpressError(400,"send valid data for listing ");
+    // }
+   let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});//(...)is a spread opertator
     
     // res.redirect("/listings");
@@ -107,6 +142,20 @@ app.delete("/listings/:id", async(req,res) => {
 //     res.send("successful testing");
 // });
 
+app.all("*",(req,res,next) => {
+    next(new ExpressError(404,"Page not found!"));
+});
+
+//custom error handling
+app.use((err,req,res,next) => {
+    let{statusCode = 500, message = "something went wrong"} = err;
+    res.status(statusCode).render("error.ejs",{message});
+    // res.render("error.ejs",{err});
+});
+
+app.listen(8080, () => {
+  console.log("app is listening to the port 8080");
+});
 
 //nmp init,express,mongoose,ejs
 
